97e35bb6c45400a966400fcf5f9a9b844a4a0a27
jarred stiger <jarredstiger@jarreds-MBP.attlocal.net>
2025-07-01 13:51:30 -0500

    fix: use NEXT_PUBLIC_API_URL for axios baseURL (production compatibility)

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://localhost:4000/',
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
   headers: {
     'Content-type': 'application/json',
   },
 });
+
+export default instance;
