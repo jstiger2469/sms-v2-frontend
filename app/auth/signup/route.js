7e90f79e9cc35768922ffe2282a6128f3ccac084
jarred stiger <jarredstiger@jarreds-MBP.attlocal.net>
2025-07-02 00:11:05 -0500

    Force Vercel redeploy with signup route fix

--- app/auth/signup/route.js
+++ app/auth/signup/route.js
@@ -1,7 +1,7 @@
 import { handleLogin } from '@auth0/nextjs-auth0'
 
 export const GET = async (request) => {
-  // Force signup by using the signup parameter
+  // Force signup by using the signup parameter - updated
   return handleLogin(request, {
     authorizationParams: {
       screen_hint: 'signup'
