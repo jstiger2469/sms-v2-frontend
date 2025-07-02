d7b23901b0080623180853685c61b5a80218651b
jarred stiger <jarredstiger@jarreds-MBP.attlocal.net>
2025-07-02 00:05:10 -0500

    Fix signup route to directly call Auth0 signup endpoint

--- app/auth/login/route.js
+++ app/auth/login/route.js
@@ -1,13 +1,5 @@
 import { handleLogin } from '@auth0/nextjs-auth0'
 
 export const GET = async (request) => {
-  // Check if this is a signup request
-  const { searchParams } = new URL(request.url)
-  const isSignup = searchParams.get('signup') === 'true'
-  
-  return handleLogin(request, {
-    authorizationParams: {
-      screen_hint: isSignup ? 'signup' : 'login'
-    }
-  })
+  return handleLogin(request)
 } 
--- app/auth/signup/route.js
+++ app/auth/signup/route.js
@@ -1,1 +1,10 @@
- 
+import { handleLogin } from '@auth0/nextjs-auth0'
+
+export const GET = async (request) => {
+  // Force signup by using the signup parameter
+  return handleLogin(request, {
+    authorizationParams: {
+      screen_hint: 'signup'
+    }
+  })
+} 
